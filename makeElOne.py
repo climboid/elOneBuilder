db6e6a8ab52988c31cdfefaaa76f9aa0722ab747
Oscar <oscar_villarreal-FIR@mckinsey.com>
2012-10-10 15:31:01 -0400

    updates

--- makeElOne.py
+++ makeElOne.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
+
 import inspect, os, errno, markup
 path =  os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+'/www/img/' # script directory
 print path
 
+counter = 1
 for f in os.listdir(path):
+	if f[0] is ".":
+		continue
 	page = markup.page()
 	page.init(charset="UTF-8")
-
-	page.img(src=f, width=1024, height=768)
-	print page
+	from markup import oneliner as e
+	page.a(e.img(src='img/'+f, width=1024), href='index'+str(counter+1)+'.html')
+	final = open('www/index'+str(counter)+'.html','w')
+	counter = counter + 1
+	final.write(str(page))
+	final.close()
+	
 
